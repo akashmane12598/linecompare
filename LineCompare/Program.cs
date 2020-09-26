aec4f7f434a414cc9d689e73e55b3ab84a9d3794
Akash <akashmane12598@gmail.com>
2020-09-26 23:16:17 +0530

    [Akash]Add. Master Code

---
+++ LineCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineCompare
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
