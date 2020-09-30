5382dae3c75972ed924c991b01f17d217a97c9a6
Akash <akashmane12598@gmail.com>
2020-09-30 22:49:29 +0530

    [Akash] Add. CompareTo UC4

--- LineCompare/Program.cs
+++ LineCompare/Program.cs
@@ -7,25 +7,31 @@ namespace LineCompare
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            Program pg = new Program();
+            pg.linecompare();
+        }
+        public void linecompare()
+        {
             Console.WriteLine("Enter the values of x1 and y1");
             double x1 = Convert.ToInt32(Console.ReadLine());
-            double y1=  Convert.ToInt32(Console.ReadLine());
+            double y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the values of x2 and y2");
             double x2 = Convert.ToInt32(Console.ReadLine());
             double y2 = Convert.ToInt32(Console.ReadLine());
-            double var = Math.Pow((x2 - x1), 2)+Math.Pow((y2-y1),2);
-            double length = Math.Sqrt(var);
-            Console.WriteLine("Length of a line1 : "+Math.Round(length,2));
+            double var = Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2);
+            string length = Math.Round(Math.Sqrt(var),2).ToString();
+            Console.WriteLine("Length of a line1 : " + length);
             Console.WriteLine("Enter the values of x3 and y3");
             double x3 = Convert.ToInt32(Console.ReadLine());
             double y3 = Convert.ToInt32(Console.ReadLine());
             double var1 = Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2);
-            double length2 = Math.Sqrt(var1);
-            if (length == length2)
+            string length2 = Math.Round(Math.Sqrt(var1),2).ToString();
+            Console.WriteLine("Length of a line2 : " + length2);
+            if (length.CompareTo(length2)==0)
             {
                 Console.WriteLine("Two lines are equal");
             }
-            else if(length>length2)
+            else if (length.CompareTo(length2) == 1)
             {
                 Console.WriteLine("Line1 is greater than Line2");
             }
